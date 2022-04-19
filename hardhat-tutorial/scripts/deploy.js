7523f4a5f9009448c69550b2c4e7f43e27cbb644
Shouryam Kumar <shouryam1508@gmail.com>
2022-04-20 01:48:35 +0530

    bug fix

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,47 +1,35 @@
 const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
-const { WHITELIST_CONTRACT_ADDRESS,  METADATA_URL } = require("../constants");
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
 async function main() {
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
+  */
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
 
-    //address of the already deployed whitelisted contract
-    const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // deploy the contract
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  ); //0x7862Ff3FEd552B5a26ACbAE04f7b17B506Dcb891
 
-    // URL from which we can extract the metadata for Crypto dev nft
-    const metadataURL = METADATA_URL;
-
-
-    //create an abstract instance for he contract to deploy
-    const cryptodevsContract = await ethers.getContractFactory("CryptoDevs");
-
-    //deploy the contract
-    const deployedCryptoDevsContract = await cryptodevsContract.deploy(
-        metadataURL,
-        whitelistContract
-    )
-
-    //wait for the contract to be deployed
-    await deployedCryptoDevsContract.deployed();
-    
-
-    //print the address of the deployed contract
-    console.log(
-        "Crypto Devs Contract Address:",
-        deployedCryptoDevsContract.address
-    );
-
-
+  // print the address of the deployed contract
+  console.log(
+    "Crypto Devs Contract Address:",
+    deployedCryptoDevsContract.address
+  );
 }
 
-//Call the main function and check if there is any error
-
+// Call the main function and catch if there is any error
 main()
-    .then(()=>[
-        process.exit(0)
-    ])
-    .catch((err)=>{
-        console.error(err);
-        process.exit(1);
-    })
-
-
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
